b2ee0137c807d55cdd43a6371679006c799b6b05
Oksana Davydovich <ks.davydovich@gmail.com>
2020-01-27 19:36:25 +0300

    Initial commit

---
+++ Epam-asp-net-mvs-course/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_asp_net_mvs_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
