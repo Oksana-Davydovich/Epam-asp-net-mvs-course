01373731551b8b814e430abb83b804eacc43fa57
Oksana Davydovich <ks.davydovich@gmail.com>
2020-01-30 20:50:16 +0300

    Tasks 1 - 5 done

--- Task5/Properties/AssemblyInfo.cs
+++ Task5/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Epam-asp-net-mvs-course")]
+[assembly: AssemblyTitle("Task5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Epam-asp-net-mvs-course")]
+[assembly: AssemblyProduct("Task5")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("048db81b-f39e-40e3-8240-2bf6137d3956")]
+[assembly: Guid("5cbaf096-6620-4531-99c8-8ffa303db8de")]
 
 // Version information for an assembly consists of the following four values:
 //
